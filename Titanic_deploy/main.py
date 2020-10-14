c3e74ed01c1d9158ab15850826c0aaefb249d7bb
jalondono <juanlondono151776@hotmail.com>
2020-10-13 20:22:21 -0500

    [upload] main.py * Model API

--- Titanic_deploy/main.py
+++ Titanic_deploy/main.py
@@ -1,20 +1,56 @@
-import typer
-
-app = typer.Typer()
-
-
-@app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
+import uvicorn
+from fastapi import FastAPI, Request
+import pickle
+import pandas as pd
 
 
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        typer.echo(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        typer.echo(f"Bye {name}!")
+def load_model():
+    data_path = 'data/titanic.zip',
+    name_file = 'test.csv',
+    modelfile = '/home/jalondono/Holberton/HandsOn-MachineLearning/Titanic_deploy/mlruns/0' \
+                '/c87244e5ca5f44848a6e99e68e80a37d/artifacts/model/model.pkl'
+    # open a file, where you stored the pickled data
+    file = open(modelfile, 'rb')
+
+    # load the model
+    model = pickle.load(file)
+
+    # close the file
+    file.close()
+    return model
+
+
+def predict(model, df):
+    """
+    make the predictions over the dataset
+    :param model:
+    :return:
+    """
+    y_pred = model.predict(df)
+    data_pred = df.copy()
+    data_pred['Survived'] = y_pred
+    print(data_pred[['Name', 'Survived']].head(20))
+
+    total = data_pred.shape[0]
+    survived = sum(data_pred['Survived'] == 1)
+    percentage = round(100 * survived / total, 2)
+
+    print(f"Survived: {survived}/{total} or {percentage}%")
+    return {"predictions": {"total": total, "survivors": survived, "Percentage": percentage}}
+
+
+app = FastAPI()
+model = load_model()
+
+
+@app.post("/")
+async def api_model(request: Request):
+    data = await request.json()
+    data = data["data"]
+    df = pd.DataFrame(data)
+    predictions = predict(model, df)
+    return predictions
 
 
 if __name__ == "__main__":
-    app()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
